2aecd3ad1dffc0a29dfd78e5a031a352689d0b45
Michael Frieze <michaelfrieze@icloud.com>
2022-03-22 23:31:33 -0400

    installed the strapi slugify plugin and configured it

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,27 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+    },
+  },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        event: {
+          field: "slug",
+          references: "name",
+        },
+      },
+    },
+  },
+});
+
 // module.exports = ({ env }) => ({
 //   // ...
 //   upload: {
@@ -16,18 +40,3 @@
 //   },
 //   // ...
 // });
-
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-    },
-  },
-  // ...
-});
--- src/api/event/content-types/event/lifecycles.js
+++ src/api/event/content-types/event/lifecycles.js
@@ -1,16 +1,16 @@
-const slugify = require("slugify");
+// const slugify = require("slugify");
 
-module.exports = {
-  beforeCreate(event) {
-    const { data } = event.params;
-    if (data.name) {
-      data.slug = slugify(data.name, { lower: true });
-    }
-  },
-  beforeUpdate(event) {
-    const { data } = event.params;
-    if (data.name) {
-      data.slug = slugify(data.name, { lower: true });
-    }
-  },
-};
+// module.exports = {
+//   beforeCreate(event) {
+//     const { data } = event.params;
+//     if (data.name) {
+//       data.slug = slugify(data.name, { lower: true });
+//     }
+//   },
+//   beforeUpdate(event) {
+//     const { data } = event.params;
+//     if (data.name) {
+//       data.slug = slugify(data.name, { lower: true });
+//     }
+//   },
+// };
