919345fbc4625ca0914abcc9fb38b836b3f2cb00
Michael Frieze <michaelfrieze@icloud.com>
2022-03-22 23:14:32 -0400

    slugify is working now

--- src/api/event/content-types/event/lifecycles.js
+++ src/api/event/content-types/event/lifecycles.js
@@ -3,14 +3,14 @@ const slugify = require("slugify");
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
-    if (data.title) {
-      data.slug = slugify(data.title, { lower: true });
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
     }
   },
   beforeUpdate(event) {
     const { data } = event.params;
-    if (data.title) {
-      data.slug = slugify(data.title, { lower: true });
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
     }
   },
 };
